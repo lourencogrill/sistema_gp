4e2ff23a50cb904615b68beb0ef2e0b819565f6e
lourencogrill <jorgelourencogrill@gmail.com>
2025-06-20 09:40:48 -0300

    Commit #19

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -14,23 +14,23 @@ export default function LoginPage() {
           onClick={() => signIn('google', { callbackUrl: '/dashboard' })}
           className="flex w-full items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
         >
-          {/* Este SVG pode ser substituído por um componente de Ícone no futuro */}
-          <svg
-            className="mr-2 h-5 w-5"
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fab"
-            data-icon="google"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 488 512"
-          >
-            <path
-              fill="currentColor"
-              d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 126 23.4 172.9 61.9l-76.4 76.4C305.5 102.7 274.5 96 248 96c-88.8 0-160.1 71.3-160.1 160s71.3 160 160.1 160c98.1 0 134.1-68.1 140.2-101.6H248v-85.3h236.1c2.3 12.7 3.9 26.9 3.9 41.4z"
-            ></path>
-          </svg>
-          Entrar com Google
+          <div className="mr-2 h-5 w-5">
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="google"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 488 512"
+            >
+              <path
+                fill="currentColor"
+                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 126 23.4 172.9 61.9l-76.4 76.4C305.5 102.7 274.5 96 248 96c-88.8 0-160.1 71.3-160.1 160s71.3 160 160.1 160c98.1 0 134.1-68.1 140.2-101.6H248v-85.3h236.1c2.3 12.7 3.9 26.9 3.9 41.4z"
+              ></path>
+            </svg>
+          </div>
+          <span>Entrar com Google</span>
         </button>
       </div>
     </div>
