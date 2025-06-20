89db05befc23d0c3e3b03a4b6d01da1e66698ba0
lourencogrill <jorgelourencogrill@gmail.com>
2025-06-20 12:16:35 -0300

    Commit #44

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -97,14 +97,18 @@ export const Sidebar = ({ isMobileMenuOpen, onMenuClose, onMenuExpandChange }: S
         />
       )}
       
-      {/* Sidebar */}
+      {/* Sidebar - VERTICAL NA ESQUERDA */}
       <aside 
         className={`
           fixed top-0 left-0 h-full bg-white border-r border-gray-200 z-50 
           transition-all duration-300 ease-in-out
           ${isMobile 
-            ? isMobileMenuOpen ? 'w-64 translate-x-0' : 'w-64 -translate-x-full'
-            : shouldShowExpanded ? 'w-64' : 'w-16'
+            ? isMobileMenuOpen 
+              ? 'w-64 translate-x-0' 
+              : 'w-64 -translate-x-full'
+            : shouldShowExpanded 
+              ? 'w-64' 
+              : 'w-16'
           }
         `}
         onMouseEnter={() => !isMobile && setIsHovered(true)}
